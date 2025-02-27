2ae8957219842f42bcb98d357a0a6871a1bae941
Tushar Machavolu <tush.machavolu@gmail.com>
2025-02-27 22:40:58 +0530

    feat: tweak weights

--- src/properties.py
+++ src/properties.py
@@ -7,10 +7,11 @@ overall_weights = {
     "score": 0.75,
 }
 gpt_category_multipliers = {
-    "indian_polity": 0.4,
-    "indian_economy": 0.25,
-    "geopolitics": 0.175,
+    "indian_polity": 0.5,
+    "indian_economy": 0.2,
+    "geopolitics": 0.125,
     "global_current_affairs": 0.125,
     "indian_local_news": 0.05,
 }
-assert sum([v for k, v in gpt_category_multipliers.items()]) == 1
+
+assert sum(gpt_category_multipliers.values()) == 1.0
