65984bf2e2b6e3e4ea81534923d3af45b29d6cd3
Tushar Machavolu <tush.machavolu@gmail.com>
2025-02-27 22:08:26 +0530

    feat: tweak weights

--- src/properties.py
+++ src/properties.py
@@ -1,11 +1,16 @@
 sentiment_threshold = 0.75
 testing_gpt_threshold = 5
-overall_weights = {"source": 0.05, "sentiment": 0.05, "text": 0.2, "score": 0.7}
+overall_weights = {
+    "source": 0.05,
+    "sentiment": 0.05,
+    "text": 0.15,
+    "score": 0.75,
+}
 gpt_category_multipliers = {
-    "indian_polity": 0.3,
-    "indian_economy": 0.3,
-    "global_current_affairs": 0.175,
+    "indian_polity": 0.4,
+    "indian_economy": 0.25,
     "geopolitics": 0.175,
+    "global_current_affairs": 0.125,
     "indian_local_news": 0.05,
 }
 assert sum([v for k, v in gpt_category_multipliers.items()]) == 1
