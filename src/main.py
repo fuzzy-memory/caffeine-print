7ee5156345beabebf853449c6c7b5512818e93bd
Tushar Machavolu <tush.machavolu@gmail.com>
2025-03-03 19:19:36 +0530

    refactor: move news retrieval function

--- src/main.py
+++ src/main.py
@@ -6,8 +6,7 @@ import ssl
 import sys
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from http import HTTPStatus
-from typing import Any, Dict
+from get_news import get_news_from_api
 
 import nltk  # type: ignore
 import pandas as pd
@@ -17,99 +16,6 @@ from dotenv import load_dotenv
 from htmls import get_formatted_date, render_news
 from ranking import rank_articles
 from similarity_clustering import deduplicate_articles
-
-
-def get_from_worldnewsapi_com(test_mode: bool = False):
-    if test_mode:
-        dir_path = "assets/test/"
-        pth = os.path.join(os.path.curdir, dir_path)
-        if "news.json" in os.listdir(pth):
-            item_count = len(json.load(open("assets/test/news.json", "r")))
-            print(f"{item_count} articles already extracted in `assets/test`")
-            return
-
-    # Set vars
-    api_key = os.environ.get("WORLDNEWSAPI_KEY")
-    url = "https://api.worldnewsapi.com/search-news"
-    # Date limit vars
-    today = datetime.date.today()
-    latest = datetime.datetime.combine(
-        date=today - datetime.timedelta(days=0), time=datetime.time(0, 0, 0)
-    )
-    earliest = datetime.datetime.combine(
-        date=today - datetime.timedelta(days=1), time=datetime.time(0, 0, 0)
-    )
-    offset = 0
-    news_items_per_call = 100
-    news_sources = ",".join(
-        [i.strip() for i in json.load(open("assets/news_sources.json", "r")).keys()]
-    )
-
-    # API ops
-    print("Sending GET requests")
-    headers = {"x-api-key": api_key}
-    params: Dict[str, Any] = {
-        "source-country": "in",
-        "language": "en",
-        "earliest-publish-date": earliest,
-        "latest-publish-date": latest,
-        "categories": "politics",
-        "number": news_items_per_call,
-        "offset": offset,
-        "sort": "publish-time",
-        "sort-direction": "DESC",
-        "news-sources": news_sources,
-    }
-
-    # First call
-    first_response = requests.get(url, headers=headers, params=params)
-    if first_response.status_code == HTTPStatus.OK:
-        news_items = first_response.json().get("news")
-        offset += news_items_per_call
-        total_items = first_response.json().get("available")
-        remaining_items = total_items - offset
-        remaining_quota = float(first_response.headers.get("X-API-Quota-Left"))  # type: ignore
-        print(
-            f"Found {first_response.json().get('available')} articles\nRetrieved {offset} items in total. {remaining_items} items to go"
-        )
-    else:
-        raise requests.HTTPError(
-            f"{first_response.status_code}: {first_response.json().get('message')}"
-        )
-
-    # Pagination calls
-    while True:
-        if test_mode:
-            break
-        if remaining_items <= 0 or remaining_quota <= 1:
-            break
-        params.update({"offset": offset, "number": news_items_per_call})
-        next_response = requests.get(url, headers=headers, params=params)
-        if next_response.status_code == HTTPStatus.OK:
-            received = len(next_response.json().get("news"))
-            if total_items - received < news_items_per_call:
-                news_items_per_call = total_items - received
-            news_items.extend(next_response.json().get("news"))
-            remaining_items -= received
-            offset += news_items_per_call
-            remaining_quota = float(first_response.headers.get("X-API-Quota-Left"))  # type: ignore
-            print(
-                f"Retrieved {len(news_items)} items in total. "
-                + (f"{remaining_items} to go" if remaining_items >= 0 else "")
-            )
-        else:
-            json.dump(news_items, open("assets/incomplete_news.json", "w"))
-            raise requests.HTTPError(
-                f"{next_response.status_code}: Retrieved until offset {offset}. Quota: {next_response.headers.get('X-API-Quota-Left')}"
-            )
-    print(f"Operation complete. Remaining quota: {remaining_quota}")
-    df = pd.DataFrame(news_items).drop_duplicates(subset=["id"])
-    path_to_write = "assets/" + ("test/" if test_mode else "") + "news.json"
-    df.to_json(
-        path_to_write,
-        index=False,
-        orient="records",
-    )
 
 
 def send_email(content: str):
@@ -157,7 +63,7 @@ if __name__ == "__main__":
         print("----- RUNNING IN TEST MODE -----")
     else:
         print("----- Running script -----")
-    get_from_worldnewsapi_com(test_mode=testing_flag)
+    get_news_from_api(caller="general", test_mode=testing_flag)
     print("----------")
     dedup_processed_news = deduplicate_articles(test_mode=testing_flag)
     print("----------")
