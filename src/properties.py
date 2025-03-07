d8f3525822b206617ebd20214730371c8653c86a
Tushar Machavolu <tush.machavolu@gmail.com>
2025-03-08 00:29:12 +0530

    feat: tweak weights

--- src/properties.py
+++ src/properties.py
@@ -6,9 +6,9 @@ testing_gpt_threshold = 5
 overall_weights = {
     "source": 0.05,
     "sentiment": 0.05,
-    "text": 0.15,
-    "score": 0.65,
-    "confidence": 0.1,
+    "text": 0.1,
+    "score": 0.75,
+    "confidence": 0.05,
 }
 gpt_category_multipliers = {
     "indian_polity": 0.5,
